0dfa23cb1a1d390841927cf9d5b9a0bf0481fe01
junnf <junnf@users.noreply.github.com>
2016-11-25 13:13:22 +0800

    test js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+
+var myObject = {
+  value: 1
+}
+
+myObject.double = function () {
+  var that = this;
+  var helper = function () {
+      that.value = add(that.value, that.value);    
+  };
+  helper();
+  alert(that.value);
+};
+
+myObject.double();
